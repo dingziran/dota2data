a0a14d12566cd9f4e105dd4bc50daa67ec889d01
Ding Ziran <dingziranrr@163.com>
2015-01-07 11:15:06 +0000

    first online line chart edition

--- client/app.js
+++ client/app.js
@@ -5,60 +5,59 @@ app.config(function($stateProvider,$httpProvider, $urlRouterProvider) {
 
       //Remove the header used to identify ajax call  that would prevent CORS from working
       delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  //
-  // For any unmatched url, redirect to /dashboard
-  $urlRouterProvider.otherwise("/dashboard");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('dashboard', {
-      url: "/dashboard",
-      templateUrl: "dashboard.html"
-    });
-});
-
-app.controller('dashboardController',function($scope){
-      //define data
-  var dataset=[1,2,3,4,5,6,5,4,3,2,1,2,3,4,5,6,7,8,9];
-
-  //define svg
-  var width=500;
-  var height=400;
-  var x=d3.scale.linear()
-          .range([0,width]);
-  var y=d3.scale.linear()
-          .range([height,0]);
-  var svg = d3.select('#barChart').append('svg')
-    .attr('width', width) 
-    .attr('height', height);
-  
-  //
-  function render(data){
-    y.domain([0,100]);
-    d3.select("svg").selectAll("rect")
-      .data(data)
-      .enter()
-      .append('rect');
-
-    d3.select("svg").selectAll("rect")
-      .data(data)
-      .attr({
-        x:function(d,i){return i*(width/data.length)},
-        y:function(d){return y(d)},
-        width:(width/data.length-3),
-        height:function(d){return height-y(d)},
-        fill:function(d){return 'rgb(0,0,'+d*3+')';}
-      });
-    d3.select("svg").selectAll("rect")
-      .data(data)
-      .exit()
-      .remove();
-  }
-  setInterval(function(){
-      console.log("refresh");
-    dataset.shift();
-    dataset.push(Math.round(Math.random()*100));
-    render(dataset);
-  },1000);
-  render(dataset);
+      //
+      // For any unmatched url, redirect to /dashboard
+      $urlRouterProvider.otherwise("/dashboard");
+      //
+      // Now set up the states
+      $stateProvider
+        .state('dashboard', {
+          url: "/dashboard",
+          templateUrl: "dashboard.html",
+          controller:function($scope){
+          //define data
+            var dataset=[1,2,3,4,5,6,5,4,3,2,1,2,3,4,5,6,7,8,9];
+          
+            //define svg
+            var width=500;
+            var height=400;
+            var x=d3.scale.linear()
+                    .range([0,width]);
+            var y=d3.scale.linear()
+                    .range([height,0]);
+            var svg = d3.select('#barChart').append('svg')
+              .attr('width', width) 
+              .attr('height', height);
+            
+            //
+            function render(data){
+              y.domain([0,100]);
+              d3.select("svg").selectAll("rect")
+                .data(data)
+                .enter()
+                .append('rect');
+          
+              d3.select("svg").selectAll("rect")
+                .data(data)
+                .attr({
+                  x:function(d,i){return i*(width/data.length)},
+                  y:function(d){return y(d)},
+                  width:(width/data.length-3),
+                  height:function(d){return height-y(d)},
+                  fill:function(d){return 'rgb(0,0,'+d*3+')';}
+                });
+              d3.select("svg").selectAll("rect")
+                .data(data)
+                .exit()
+                .remove();
+            }
+            // setInterval(function(){
+            //     console.log("refresh");
+            //   dataset.shift();
+            //   dataset.push(Math.round(Math.random()*100));
+            //   render(dataset);
+            // },1000);
+            render(dataset);
+          }
+        });
 });
--- client/app/info/info.js
+++ client/app/info/info.js
@@ -11,27 +11,135 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "app/info/info.html",
       controller: function($scope,dotaService) {
         $scope.online=0;
+        $scope.data=[];
+        $scope.stop=0;
+        
         $scope.update=function(){
-            
-             $.ajax({
-                 type: "GET",
-                 url: "https://api.mongolab.com/api/1/databases?apiKey=U0ILXvPGN6SC7otpblL5agQ2D7YQikuB",
-                 dataType: "json",
-                 success: function(data){
-                     console.log(data);
-                }
-             });    
-            
-            
-            
-            
-            
-            
-            dotaService.getOnlineNumber().success(function(data){
-                $scope.online=data.response.player_count;
-                console.log(data);
-            })    
+            if($scope.stop==0){
+                $scope.stop=setInterval(function(){
+                    console.log("after 5 minutes");
+                    // $scope.data.push({
+                    //     date:new Date(),
+                    //     close:Math.random()*10
+                    // });
+                    // render($scope.data);
+                    
+                    
+                    dotaService.getOnlineNumber().success(function(data){
+                        $scope.online=data.response.player_count;
+                        $scope.data.push({
+                            date:new Date(),
+                            close:data.response.player_count+Math.random()*10
+                        });
+                        
+                        render($scope.data);
+                    })    
+                    
+                },300000);
+            }
+            else{
+                clearInterval($scope.stop);
+                $scope.stop=0;
+            }
+            // dotaService.getOnlineNumber().success(function(data){
+            //     $scope.online=data.response.player_count;
+            //     $scope.data.push({
+            //         date:new Date(),
+            //         close:data.response.player_count
+            //     });
+                
+            //     render(newData);
+            // })    
         }
+        
+        // $scope.draw=function(){
+        //     var len=Math.random()*10;
+        //     console.log(len);
+        //     for(var i=0;i<len;i++){
+        //         newData.push({date:i,close:2*i});
+        //     }
+        //     render(newData);
+            
+        // }
+        
+        
+        
+        // for(var i=0;i<10;i++){
+        //     $scope.data.push({date:i,close:2*i});
+        // }
+        
+
+            
+        var margin = {top: 20, right: 20, bottom: 30, left: 100},
+            width = 600 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+            
+        var x = d3.time.scale()
+            .range([0, width]);
+        
+        var y = d3.scale.linear()
+            .range([height, 0]);
+            
+        var format = d3.time.format("%X");
+        
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .ticks(5)
+            .tickFormat(function(v){
+                return format(v);
+            });
+        
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+        
+        var line = d3.svg.line()
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.close); });
+        
+        var svg = d3.select("#chart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            
+        function render(data){  
+            // console.log('render')
+            
+          x.domain(d3.extent(data, function(d) { return d.date; }));
+          y.domain(d3.extent(data, function(d) { return d.close; }));
+          
+            
+          svg.selectAll("g").remove();
+          
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+        
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Players");  
+            
+          svg.selectAll("path.line")
+              .data([data])
+              .enter()
+              .append("path")
+              .attr("class", "line")
+              .attr("d", line);
+          svg.selectAll("path.line")
+              .data([data])
+              .attr("d", line);
+        }
+        
+        // render(data);
       }
     });
 });
